b6f36f2ffca529bec93273dfa550d5df4df8b849
Yousef Altinawi <Yousef.Altinawi@smail.inf.h-brs.de>
2023-10-22 23:10:32 +0200

    Übung 2

---
+++ test/org/hbrs/se1/ws23/uebung2/ContainerTest.java
@@ -0,0 +1,24 @@
+package org.hbrs.se1.ws23.uebung2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @Test
+    void addMember() {
+    }
+
+    @Test
+    void dump() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+}
